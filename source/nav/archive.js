f7ac4608e7a4007bdf4df9755a2e100e62a6292f
Perry Mitchell <perry@perrymitchell.net>
2016-07-20 00:10:04 +0300

    Presentation of entries

--- source/nav/archive.js
+++ source/nav/archive.js
@@ -1,7 +1,23 @@
+const tuck = require("tuck");
 const menu = require("../tools/menu.js");
 
 const ARCHIVE_BACK =    { title: "Back", value: "+back" };
+const ARCHIVE_CLOSE =   { title: "Close", value: "+close" };
 const ARCHIVE_TOROOT =  { title: "Root", value: "+root" };
+
+function renderEntry(entry) {
+    console.log(tuck(
+        [
+            entry.getProperty("title"),
+            "───",
+            `Username:   ${entry.getProperty("username")}`,
+            "Password:   ********"
+        ], {
+            center: false,
+            textCenter: false
+        }
+    ));
+}
 
 module.exports = function initWithWorkspace(workspace) {
     let archive = workspace.getArchive(),
@@ -17,11 +33,17 @@ module.exports = function initWithWorkspace(workspace) {
                 return;
             } else if (action === "+back") {
                 currentNode = nodeChain.pop();
+            } else if (action === "+root") {
+                currentNode = archive;
+                nodeChain = [];
             } else {
                 let [context, id] = action.split(":");
                 if (context === "g") {
                     nodeChain.push(currentNode);
                     currentNode = archive.getGroupByID(id);
+                } else if (context === "e") {
+                    let entry = archive.getEntryByID(id);
+                    renderEntry(entry);
                 }
             }
             return archiveHandler.presentCurrentNode();
@@ -29,15 +51,25 @@ module.exports = function initWithWorkspace(workspace) {
 
         presentCurrentNode: function() {
             let menuItems = [],
-                groups = currentNode.getGroups();
-            if (currentNode !== archive) {
+                isRoot = currentNode === archive,
+                groups = currentNode.getGroups(),
+                entries = isRoot ? [] : currentNode.getEntries();
+            if (isRoot) {
+                menuItems.push(ARCHIVE_CLOSE);
+            } else {
                 menuItems.push(ARCHIVE_BACK);
                 menuItems.push(ARCHIVE_TOROOT);
             }
             groups.forEach(function(group) {
                 menuItems.push({
-                    title: `/${group.getTitle()}/`,
+                    title: `» ${group.getTitle()}`,
                     value: `g:${group.getID()}`
+                });
+            });
+            entries.forEach(function(entry) {
+                menuItems.push({
+                    title: `› ${entry.getProperty("title")}`,
+                    value: `e:${entry.getID()}`
                 });
             });
             return menu
