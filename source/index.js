2e49273f994430a3fb80a6c096bbd93ff33b6a78
Perry Mitchell <perry@perrymitchell.net>
2016-08-04 20:40:26 +0300

    Support specifying archive filename at command line

--- source/index.js
+++ source/index.js
@@ -2,22 +2,25 @@
 
 "use strict";
 
+const argv = require("minimist")(process.argv.slice(2));
+
+const openArchiveHandler = require("./nav/open-archive.js");
 const mainMenu = require("./nav/main-menu.js");
 
-process.on('uncaughtException', function (err) {
-  console.log('Caught exception: ' + err);
-});
-process.on("exit", function() {
-    console.log("exit", arguments);
-});
+let archiveFilename = argv._[0];
+
+if (archiveFilename) {
+    return openArchiveHandler
+        .openFile(archiveFilename);
+}
 
 return mainMenu
     .presentMenu()
     .then(function() {
-        console.log("Ending");
+        console.log("Goodbye.");
     })
     .catch(function(err) {
-        console.error("Process failed");
+        console.error("Process failed...");
         setTimeout(function() {
             throw err;
         }, 0);
